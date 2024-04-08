aea64df7c55c342ad5f61f3f95274d35fd9b2900
izabelle-g <aguev976@mtroyal.ca>
2024-04-07 21:47:39 -0600

    Connect to supabase start files

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,6 +2,15 @@ import Header from './Header.jsx';
 import RaceList from './RaceList.jsx';
 import Display from './Display.jsx';
 
+const express = require('express');
+const app = express();
+const router = require('f1-dashboard/src/f1-router.js');
+
+app.listen(8080, () => {
+    console.log('listening on port 8080');
+    console.log('http://localhost:8080/f1/status');
+});
+
 const Dashboard = (props) => {
     // TODO: organize data from supabase
     // TODO: change display area
---
+++ src/f1-router.js
@@ -0,0 +1,5 @@
+const supa = require('@supabase/supabase-js');
+const supaUrl = process.env.URL;
+const supaAnonKey = process.env.API_KEY;
+
+const supabase = supa.createClient(supaUrl, supaAnonKey);
